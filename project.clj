970ac13369fcaad47330169808668213e666ccb9
franks42 <frank.siebenlist@gmail.com>
2012-04-04 23:04:58 -0700

    1.1.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject clj-ns-browser "1.0.0"
-  :description "Smalltalk-like namespace/class/var/function browser."
+(defproject clj-ns-browser "1.1.0-SNAPSHOT"
+  :description "Smalltalk-like namespace/class/var/function browser for Clojure."
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [seesaw "1.4.0"]
                  [clj-info "0.2.2"]
