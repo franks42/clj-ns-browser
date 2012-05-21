bbbc74ef21c4aea2d26fcf716de11019f65b0a09
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2012-05-21 00:07:05 -0700

    Memoize calls to get-source
    Need a way to invalidate the memoization cache when source code can
    change.

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [franks42/seesaw "1.4.2-SNAPSHOT"]
+                 [org.clojure/core.memoize "0.5.1"]
                  [org.clojure/tools.namespace "0.1.2"]
                  [clj-info "0.2.3"]
                  [org.clojure/tools.trace "0.7.3"]
