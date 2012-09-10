5ec994ad36356a186505e6d259d509ff95547292
franks42 <frank.siebenlist@gmail.com>
2012-09-10 12:06:44 -0700

    removed 2 commented-out reps from project.clj

--- project.clj
+++ project.clj
@@ -5,12 +5,10 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [seesaw "1.4.2"]
-                 ;;[franks42/seesaw "1.4.2-SNAPSHOT"]
                  [org.clojure/tools.namespace "0.1.3"]
                  [clj-info "0.2.4"]
                  [org.clojure/tools.trace "0.7.3"]
                  [clojure-complete "0.2.1" :exclusions [org.clojure/clojure]]
-                 ;;[org.fife.ui/rsyntaxtextarea "2.0.3"]
                  [org.thnetos/cd-client "0.3.5"]]
  	:dev-dependencies [[lein-marginalia "0.7.1"]
  	                   ;[franks42/debug-repl "0.3.1-FS"]
